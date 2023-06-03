1920c8b037a1ff80b872f56705ef00db2c048f0f
swimhub <135133943+SWIMHUBISWIMMING@users.noreply.github.com>
2023-06-03 17:39:36 +0900

    Update and rename UserID to UserID.lua

---
+++ UserID.lua
@@ -0,0 +1,9 @@
+local plrs = game:GetService("Players")
+return {
+  [1230814] = plrs:GetNameFromUserIdAsync(1230814), -- bubble8gum
+  [12308148] = plrs:GetNameFromUserIdAsync(12308148), -- iittixiiixttitiixxtx
+  [870077694] = plrs:GetNameFromUserIdAsync(870077694), -- ToxicFlump123
+  [3317198400] = plrs:GetNameFromUserIdAsync(3317198400), -- IiminaI_space
+  [528997513] = plrs:GetNameFromUserIdAsync(528997513), -- Pozzews
+  [1688180916] = plrs:GetNameFromUserIdAsync(1688180916), -- 0x448
+}
