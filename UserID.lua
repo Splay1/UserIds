63b6aafc6e0b65bbfe4315a4267142937fb73c6f
Splay1 <122415144+Splay1@users.noreply.github.com>
2023-06-03 12:07:30 +0100

    UserID.lua

--- UserID.lua
+++ UserID.lua
@@ -1,6 +1,6 @@
 local plrs = game:GetService("Players")
 local t = {}
-local userids = {1230814,12308148,870077694,3317198400,528997513,1688180916} -- bubble8gum, iittixiiixttitiixxtx, ToxicFlump123, IiminaI_space, Pozzews, 0x448
+local userids = {1230814,12308148,870077694,3317198400,528997513,1688180916,4672346649} -- bubble8gum, iittixiiixttitiixxtx, ToxicFlump123, IiminaI_space, Pozzews, 0x448,ISH00TNGAS4FUN
 for i, v in pairs(userids) do
     local temp = plrs:GetNameFromUserIdAsync(v)
     table.insert(t, temp)
