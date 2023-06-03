d2573e6698b0596d1a82de3830c761e48eac4559
swimhub <135133943+SWIMHUBISWIMMING@users.noreply.github.com>
2023-06-03 19:33:06 +0900

    Update UserID.lua

--- UserID.lua
+++ UserID.lua
@@ -1,9 +1,8 @@
 local plrs = game:GetService("Players")
-return {
-  [1230814] = plrs:GetNameFromUserIdAsync(1230814), -- bubble8gum
-  [12308148] = plrs:GetNameFromUserIdAsync(12308148), -- iittixiiixttitiixxtx
-  [870077694] = plrs:GetNameFromUserIdAsync(870077694), -- ToxicFlump123
-  [3317198400] = plrs:GetNameFromUserIdAsync(3317198400), -- IiminaI_space
-  [528997513] = plrs:GetNameFromUserIdAsync(528997513), -- Pozzews
-  [1688180916] = plrs:GetNameFromUserIdAsync(1688180916), -- 0x448
-}
+local t = {}
+local userids = {1230814,12308148,870077694,3317198400,528997513,1688180916} -- bubble8gum, iittixiiixttitiixxtx, ToxicFlump123, IiminaI_space, Pozzews, 0x448
+for i, v in pairs(userids) do
+    local temp = plrs:GetNameFromUserIdAsync(v)
+    table.insert(t, temp)
+end
+return t
